620cc50b06eaa103e6425ea025c4f4f4e4465ed0
Krishan Koenig <krishan.koenig@googlemail.com>
2019-01-10 12:41:00 +0100

    publish migration file if not already published

--- src/MultitenancyServiceProvider.php
+++ src/MultitenancyServiceProvider.php
@@ -2,17 +2,28 @@
 
 namespace RomegaDigital\Multitenancy;
 
+use Illuminate\Support\Collection;
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
 use RomegaDigital\Multitenancy\Contracts\Tenant as TenantContract;
 
 class MultitenancyServiceProvider extends ServiceProvider
 {
-    public function boot()
+    /**
+     * Bootstrap the package services.
+     *
+     * @return void
+     */
+    public function boot(Filesystem $filesystem)
     {
-        $this->loadMigrationsFrom(realpath(__DIR__.'/../migrations'));
+        $this->loadMigrationsFrom(realpath(__DIR__ . '/../migrations'));
 
         $this->publishes([
-            __DIR__.'/../config/multitenancy.php' => config_path('multitenancy.php'),
+            __DIR__ . '/../migrations/create_tenants_table.php' => $this->getMigrationFileName($filesystem),
+        ], 'migrations');
+
+        $this->publishes([
+            __DIR__ . '/../config/multitenancy.php' => config_path('multitenancy.php'),
         ], 'config');
 
         $this->registerModelBindings();
@@ -20,7 +31,7 @@ class MultitenancyServiceProvider extends ServiceProvider
         $this->app->singleton(Multitenancy::class, function () {
             return new Multitenancy();
         });
-        
+
         $this->app->alias(Multitenancy::class, 'multitenancy');
     }
 
@@ -32,13 +43,35 @@ class MultitenancyServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../config/multitenancy.php',
+            __DIR__ . '/../config/multitenancy.php',
             'multitenancy'
         );
     }
 
+    /**
+     * Register model bindings.
+     *
+     * @return void
+     */
     protected function registerModelBindings()
     {
         $this->app->bind(TenantContract::class, $this->app->config['multitenancy.tenant_model']);
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @param Filesystem $filesystem
+     * @return string
+     */
+    protected function getMigrationFileName(Filesystem $filesystem): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        return Collection::make($this->app->databasePath() . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem) {
+                return $filesystem->glob($path . '*_create_tenants_table.php');
+            })->push($this->app->databasePath() . "/migrations/{$timestamp}_create_tenants_table.php")
+            ->first();
     }
 }
