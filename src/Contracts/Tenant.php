4836203e3e3d45ff402eff671f5b2d91c2aeb58f
Braden Keith <bkeith@romegadigital.com>
2019-01-11 11:42:43 -0500

    Verbiage improvements

--- src/Contracts/Tenant.php
+++ src/Contracts/Tenant.php
@@ -7,14 +7,14 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 interface Tenant
 {
     /**
-     * A Tenant belongs to many users
+     * A Tenant belongs to many users.
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
     public function users(): BelongsToMany;
 
     /**
-     * Find a Tenant by its domain
+     * Find a Tenant by its domain.
      *
      * @param string $domain
      *
