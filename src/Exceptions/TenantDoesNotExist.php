85a3f5e01841374191d1391000a6d86aeff942e9
Braden Keith <bkeith@romegadigital.com>
2019-01-11 12:13:18 -0500

    Added comments

--- src/Exceptions/TenantDoesNotExist.php
+++ src/Exceptions/TenantDoesNotExist.php
@@ -6,6 +6,9 @@ use InvalidArgumentException;
 
 class TenantDoesNotExist extends InvalidArgumentException
 {
+	/**
+	 * A Tenant does not exist at the supplied domain.
+	 */
     public static function forDomain(string $domain)
     {
         return new static("There is no tenant at domain `{$domain}`.");
--- src/Exceptions/UnauthorizedException.php
+++ src/Exceptions/UnauthorizedException.php
@@ -6,6 +6,10 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class UnauthorizedException extends HttpException
 {
+	/**
+	 * A user does not have valid permissions to access
+	 * the supplied domain.
+	 */
 	public static function forDomain(string $domain)
 	{
 		$message = "The authenticated user does not have access to domain `{$domain}`.";
@@ -15,6 +19,9 @@ class UnauthorizedException extends HttpException
 		return $exception;
 	}
 
+	/**
+	 * A user is not logged in.
+	 */
 	public static function notLoggedIn(): self
 	{
 	    return new static(403, 'User is not logged in.', null, []);
