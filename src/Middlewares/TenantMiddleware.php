a6b074523c997648f96faee8424c65b7def4b68a
Krishan Koenig <krishan.koenig@googlemail.com>
2019-01-11 14:52:31 +0100

    refactoring middleware

--- src/Middlewares/TenantMiddleware.php
+++ src/Middlewares/TenantMiddleware.php
@@ -4,35 +4,63 @@ namespace RomegaDigital\Multitenancy\Middlewares;
 
 use Closure;
 use Illuminate\Http\Request;
+use RomegaDigital\Multitenancy\Multitenancy;
+use RomegaDigital\Multitenancy\Models\Tenant;
 use RomegaDigital\Multitenancy\Exceptions\UnauthorizedException;
 
 class TenantMiddleware
 {
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param \Illuminate\Http\Request $request
-	 * @param \Closure                 $next
-	 * @param string|null              $guard
-	 *
-	 * @return mixed
-	 */
-	public function handle(Request $request, Closure $next)
-	{
-        if (app('auth')->guest()) {
+    /**
+     * @var RomegaDigital\Multitenancy\Multitenancy
+     */
+    protected $multitenancy;
+
+    /**
+     * Create new TenantMiddleware instance.
+     *
+     * @param Multitenancy $multitenancy
+     */
+    public function __construct(Multitenancy $multitenancy)
+    {
+        $this->multitenancy = $multitenancy;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     * @param string|null              $guard
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (auth()->guest()) {
             throw UnauthorizedException::notLoggedIn();
         }
 
-        $domain = app('multitenancy')->getCurrentSubDomain();
-        $tenant = app('multitenancy')->getTenantClass()::findByDomain($domain);
+        $domain = $this->multitenancy->getCurrentSubDomain();
+        $tenant = $this->multitenancy->getTenantClass()::findByDomain($domain);
 
-        if($tenant && $tenant->users->contains(app('auth')->user()->id)) {
-
-        	app('multitenancy')->setTenant($tenant)->applyTenantScopeToDeferredModels();
-
-        	return $next($request);
+        if (!$this->authorizedToAccessTenant($tenant)) {
+            throw UnauthorizedException::forDomain($tenant->domain);
         }
 
-    	throw UnauthorizedException::forDomain($tenant->domain);
-	}
+        $this->multitenancy->setTenant($tenant)->applyTenantScopeToDeferredModels();
+
+        return $next($request);
+    }
+
+    /**
+     * Check if user is authorized to access tenant's domain.
+     *
+     * @param RomegaDigital\Multitenancy\Models\Tenant $tenant
+     *
+     * @return boolean
+     */
+    protected function authorizedToAccessTenant(Tenant $tenant)
+    {
+        return $tenant && $tenant->users->contains(auth()->user()->id);
+    }
 }
