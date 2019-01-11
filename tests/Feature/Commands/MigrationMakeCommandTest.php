7c7b3d276e47402a273e88f9e8290997e48c244e
Naoray Kroaths <Naoray@users.noreply.github.com>
2019-01-11 15:30:18 +0100

    Feature/add create migration command (#3)
    * remove vendor folder
    
    * add vendor folder to gitignore
    
    * publish migration file if not already published
    
    * add migration command for multitenancy field
    
    * update Readme
    
    * add test for migration command
    
    * fix test errors, resulted by renaming migration file
    
    * add tests for migration command
    
    * replace down table name with DummyTable
    
    * change type of migration file created

---
+++ tests/Feature/Commands/MigrationMakeCommandTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace RomegaDigital\Multitenancy\Tests\Feature\Commands;
+
+use RomegaDigital\Multitenancy\Tests\TestCase;
+
+class MigrationMakeCommandTest extends TestCase
+{
+    /** @test */
+    public function it_adds_a_new_migration_with_tenant_id_to_the_specified_table()
+    {
+        $this->artisan('multitenancy:migration', ['name' => 'products'])
+            ->expectsOutput('Multitenancy migration created successfully.');
+    }
+}
