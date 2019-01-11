83e6e2f1691dd7c93c4d9b32ea6abb11fd236e47
Krishan Koenig <krishan.koenig@googlemail.com>
2019-01-11 17:01:08 +0100

    WIP - still have to figure out how to resolve conflict in classes

---
+++ tests/Feature/Commands/InstallCommandTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace RomegaDigital\Multitenancy\Tests\Feature\Commands;
+
+use RomegaDigital\Multitenancy\Tests\TestCase;
+
+class InstallCommandTest extends TestCase
+{
+    /** @test */
+    public function it_published_and_migrates_required_migrations_and_creates_admin_role_and_tenant()
+    {
+        $this->artisan('multitenancy:install')
+            ->expectsOutput('Publishing required Migations...')
+            ->expectsOutput('Migrations published!')
+            ->expectsOutput('Adding `Super Administrator` Role...')
+            ->expectsOutput('Role `Super Administrator` created')
+            ->expectsOutput('Adding `admin` domain...')
+            ->expectsOutput('Admin domain added successfully!');
+    }
+}
