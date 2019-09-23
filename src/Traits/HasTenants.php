c1437eebdc47232b5551e4ebe5704237cad96319
Krishan Koenig <krishan.koenig@googlemail.com>
2019-09-23 15:59:40 +0200

    WIP

--- src/Traits/HasTenants.php
+++ src/Traits/HasTenants.php
@@ -2,8 +2,26 @@
 
 namespace RomegaDigital\Multitenancy\Traits;
 
+use RomegaDigital\Multitenancy\Multitenancy;
+
 trait HasTenants
 {
+    /**
+     * Add the current tenant to the created user tenants.
+     */
+    public static function bootHasTenants()
+    {
+        static::created(function ($model) {
+            if ($model->tenants->count() > 0) {
+                return;
+            }
+
+            $model->tenants()->save(
+                resolve(Multitenancy::class)->currentTenant()
+            );
+        });
+    }
+
     /**
      * The model belongs to many tenants.
      *
